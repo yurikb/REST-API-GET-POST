de01045dfd3b99290381c7992476b1650a5895e2
yurikb <yuri.kollerborba@gmail.com>
2022-04-13 17:06:00 -0300

    first commit

---
+++ src/test/java/br/com/algaworks/crm/AlgacrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.algaworks.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgacrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
